47b9b1e0b42369b0a9005c51de4c35141e98ddbd
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-11 22:21:54 +0300

    Add model UserGroupModel

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,7 +1,22 @@
+from sqlalchemy import Column, Integer, Enum
 from enum import StrEnum, auto
+
+from src.database.models.base import Base
 
 
 class UserGroupEnum(StrEnum):
     USER = auto()
     MODERATOR = auto()
     ADMIN = auto()
+
+
+class UserGroupModel(Base):
+    __tablename__ = "user_groups"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(
+        Enum(UserGroupEnum),
+        default=UserGroupEnum.USER,
+        nullable=False,
+        unique=True
+    )
