232c445fd29ee8dc91647d51710b35033ce1415c
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-12 16:30:49 +0300

    Add FastAPI router for accounts endpoints

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 
+from src.routers import accounts
+
 
 app = FastAPI(
     title="Online Cinema API",
     version="1.0.0",
     description="Manage your purchased movies, user credentials and more.",
+)
+
+api_prefix = "/api"
+
+app.include_router(
+    accounts.router,
+    prefix=f"{api_prefix}/accounts",
+    tags=["accounts",]
 )
