1a84eff5f4ea07626b2b9539e6d74dc2b0ae7759
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-12 21:01:01 +0300

    Add endpoint register_user

--- src/routers/accounts.py
+++ src/routers/accounts.py
@@ -1,4 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.schemas.accounts import (
+    UserRegistrationRequestSchema,
+    UserRegistrationResponseSchema
+)
+from src.database.dependencies import get_db
+from src.crud.accounts import create_user
 
 
 router = APIRouter()
+
+
+@router.post(
+    "/register/",
+    status_code=status.HTTP_201_CREATED
+)
+async def register_user(
+    data: UserRegistrationRequestSchema,
+    db: AsyncSession = Depends(get_db)
+) -> UserRegistrationResponseSchema:
+    user = await create_user(db, data)
+    return UserRegistrationResponseSchema(
+        id=user.id,
+        email=user.email,
+    )
