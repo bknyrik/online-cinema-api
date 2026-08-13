35755f2aea433a2aa628c41ccec8adfb0bbd83d4
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-13 09:11:35 +0300

    Add error handling if user exists in register_user

--- src/routers/accounts.py
+++ src/routers/accounts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.schemas.accounts import (
@@ -6,7 +6,7 @@ from src.schemas.accounts import (
     UserRegistrationResponseSchema
 )
 from src.database.dependencies import get_db
-from src.crud.accounts import create_user
+from src.crud.accounts import create_user, get_user_by_email
 
 
 router = APIRouter()
@@ -20,6 +20,14 @@ async def register_user(
     data: UserRegistrationRequestSchema,
     db: AsyncSession = Depends(get_db)
 ) -> UserRegistrationResponseSchema:
+    user = await get_user_by_email(db, data.email)
+
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"User with this email {repr(data.email)} exists."
+        )
+
     user = await create_user(db, data)
     return UserRegistrationResponseSchema(
         id=user.id,
