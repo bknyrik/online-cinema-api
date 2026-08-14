ab2d3a6da4225189edc3601e7dc84d2c711f8b2a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-14 12:14:00 +0300

    Add function send_activation_email to send activation link

---
+++ src/smtp/emails.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from src.smtp.utils import send_email, parse_html_content
+
+
+async def send_activation_email(email: str, link: str, token: str) -> None:
+    html_body = parse_html_content(
+        path=Path("templates/account_activation.html"),
+        link=link,
+        token=token
+    )
+    await send_email(
+        sender="online.cinema@mail.com",
+        receiver=email,
+        subject="Account activation",
+        html_body=html_body
+    )
