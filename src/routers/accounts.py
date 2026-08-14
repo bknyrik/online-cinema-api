307d9f19e277bb9cf29c3fbd68d36927fe51e974
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-14 12:24:34 +0300

    Add feature to send email with activation link and token in register_user

--- src/routers/accounts.py
+++ src/routers/accounts.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi import APIRouter, status, Depends, HTTPException, BackgroundTasks
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database.models.accounts import UserModel
+from src.database.models.accounts import (
+    UserModel,
+    ActivationTokenModel
+)
 from src.schemas.accounts import (
     UserRegistrationRequestSchema,
     UserRegistrationResponseSchema
 )
 from src.database.dependencies import get_db
-from src.crud.accounts import create_user, get_user_by_email
+from src.crud.accounts import create_user, get_user_by_email, create_token
+from src.smtp import emails
 
 
 router = APIRouter()
@@ -20,6 +24,7 @@ router = APIRouter()
 )
 async def register_user(
     data: UserRegistrationRequestSchema,
+    background_tasks: BackgroundTasks,
     db: AsyncSession = Depends(get_db)
 ) -> UserModel:
     user = await get_user_by_email(db, data.email)
@@ -31,4 +36,17 @@ async def register_user(
         )
 
     user = await create_user(db, data)
+    activation_token = await create_token(
+        db=db,
+        user_id=user.id,
+        token_model=ActivationTokenModel
+    )
+    activation_link = "http://127.0.0.1:8000/api/accounts/activate/"
+
+    background_tasks.add_task(
+        emails.send_activation_email,
+        user.email,
+        activation_link,
+        activation_token.token
+    )
     return user
