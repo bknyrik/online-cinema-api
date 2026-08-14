f85e0093df9ae57240738aeb38245b2d68b9ab3b
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-14 21:52:18 +0300

    Add options beat_dburi and beat_scheduler to Celery app

--- src/celery_beat/app.py
+++ src/celery_beat/app.py
@@ -1,6 +1,7 @@
 from celery import Celery
 
 from src.settings import settings
+from src.database.config import SQLALCHEMY_DATABASE_CELERY_BEAT_URL
 
 
 app = Celery(
@@ -8,3 +9,7 @@ app = Celery(
     broker=settings.CELERY_BROKER_URL,
     backend=settings.CELERY_RESULT_BACKEND
 )
+
+
+app.conf.beat_dburi = SQLALCHEMY_DATABASE_CELERY_BEAT_URL
+app.conf.beat_scheduler = "sqlalchemy_celery_beat.schedulers:DatabaseScheduler"
