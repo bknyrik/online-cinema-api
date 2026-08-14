e31a4466aff70606d7f7f3afd3e7869de4fa6ae8
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-14 12:35:11 +0300

    Fix wrong path to template accounts_activation.html in send_activation_email

--- src/smtp/emails.py
+++ src/smtp/emails.py
@@ -5,7 +5,7 @@ from src.smtp.utils import send_email, parse_html_content
 
 async def send_activation_email(email: str, link: str, token: str) -> None:
     html_body = parse_html_content(
-        path=Path("templates/account_activation.html"),
+        path=Path("src/smtp/templates/account_activation.html"),
         link=link,
         token=token
     )
