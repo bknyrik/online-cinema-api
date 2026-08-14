b34fd359e0177d05ed21af45739ae802bedb59c2
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-14 12:08:18 +0300

    Add parameter subject to send_email and an email message

--- src/smtp/utils.py
+++ src/smtp/utils.py
@@ -5,7 +5,12 @@ import aiosmtplib
 from src.settings import settings
 
 
-async def send_email(sender: str, receiver: str, html_body: str) -> None:
+async def send_email(
+    sender: str,
+    receiver: str,
+    subject: str,
+    html_body: str
+) -> None:
     async with aiosmtplib.SMTP(
         hostname=settings.SMTP_HOST,
         port=settings.SMTP_PORT
@@ -15,6 +20,7 @@ async def send_email(sender: str, receiver: str, html_body: str) -> None:
         message = EmailMessage()
         message["from"] = sender
         message["to"] = receiver
+        message["subject"] = subject
 
         message.set_content(html_body, "html")
         await smtp_server.send_message(message)
