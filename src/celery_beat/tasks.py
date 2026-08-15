51b92286a587c83dfaabc069cd9f763c41ffb636
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-15 19:57:16 +0300

    Make delete_expired_activation_token synchronous

--- src/celery_beat/tasks.py
+++ src/celery_beat/tasks.py
@@ -2,15 +2,15 @@ from celery import shared_task
 
 from sqlalchemy import delete
 
-from src.database.config import SessionLocal
 from src.database.models.accounts import ActivationTokenModel
+from src.database.config import SyncSessionLocal
 
 
 @shared_task
-async def delete_expired_activation_token(user_id: int) -> None:
-    async with SessionLocal() as session:
-        await session.execute(
+def delete_expired_activation_token(user_id: int) -> None:
+    with SyncSessionLocal as session:
+        session.execute(
             delete(ActivationTokenModel)
             .where(ActivationTokenModel.user_id == user_id)
         )
-        await session.commit()
+        session.commit()
