47255ed518586812137b72ad4b1000d50f7f0768
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-16 07:50:31 +0300

    Fix call of SyncSessionLocal in context manager in delete_expired_activation_token

--- src/celery_beat/tasks.py
+++ src/celery_beat/tasks.py
@@ -8,7 +8,7 @@ from src.database.config import SyncSessionLocal
 
 @shared_task
 def delete_expired_activation_token(user_id: int) -> None:
-    with SyncSessionLocal as session:
+    with SyncSessionLocal() as session:
         session.execute(
             delete(ActivationTokenModel)
             .where(ActivationTokenModel.user_id == user_id)
