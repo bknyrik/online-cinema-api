a45410f38c27d2dad67680cba2923500a0eff4bb
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-16 10:06:41 +0300

    Add validator method validate_email in UserRegistrationRequestSchema

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,12 +1,18 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
 
 from src.database.models.accounts import UserGroupEnum
+from src.database.validators import accounts
 
 
 class UserRegistrationRequestSchema(BaseModel):
     email: EmailStr
     password: str
     group: UserGroupEnum = UserGroupEnum.USER
+
+    @field_validator("email", mode="before")
+    @classmethod
+    def validate_email(cls, value: str) -> str:
+        return accounts.validate_email(value)
 
 
 class UserRegistrationResponseSchema(BaseModel):
