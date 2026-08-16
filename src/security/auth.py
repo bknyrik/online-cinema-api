40dd5ef8d3461d7c865dba0d3cb9d89521f9c588
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-16 11:22:09 +0300

    Change parameters of create_access_token

--- src/security/auth.py
+++ src/security/auth.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone, timedelta
+from datetime import datetime
 
 from jose import jwt
 
@@ -8,12 +8,9 @@ from src.settings import settings
 ALGORITHM = "HS256"
 
 
-def create_access_token(user_id: int) -> str:
+def create_access_token(email: str, expires_at: datetime) -> str:
     return jwt.encode(
-        claims={
-            "user_id": user_id,
-            "expires_at": datetime.now(timezone.utc) + timedelta(minutes=30),
-        },
+        claims={"sub": email, "exp": expires_at},
         key=settings.JWT_SECRET_KEY,
         algorithm=ALGORITHM
     )
