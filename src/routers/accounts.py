dad0c1eb2cdd61913b7f26fa968b2a0fdbec5771
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-17 21:30:45 +0300

    Add endpoint refresh_access_token

--- src/routers/accounts.py
+++ src/routers/accounts.py
@@ -17,6 +17,8 @@ from src.schemas.accounts import (
     ChangeUserPasswordRequestSchema,
     ResetUserPasswordRequestSchema,
     ResetPasswordCompleteRequestSchema,
+    RefreshAccessTokenRequestSchema,
+    RefreshAccessTokenResponseSchema,
     MessageResponseSchema
 )
 from src.security.password import verify_password
@@ -278,6 +280,46 @@ async def reset_password_complete(
     await delete_token(db, user.id, PasswordResetTokenModel)
 
     return {"message": "Password is changed successfully"}
+
+
+@router.post(
+    "/refresh_access_token/",
+    response_model=RefreshAccessTokenResponseSchema
+)
+async def refresh_access_token(
+    data: RefreshAccessTokenRequestSchema,
+    db: AsyncSession = Depends(get_db),
+) -> dict:
+    user = await get_user_by_email(db, data.email)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    refresh_token_instance = await get_token_by_user_id(
+        db=db,
+        user_id=user.id,
+        token_model=RefreshTokenModel
+    )
+
+    if (
+        not refresh_token_instance
+        or refresh_token_instance.token != data.refresh_token
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Token is invalid or missing"
+        )
+
+    if refresh_token_instance.has_expired:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Token is expired"
+        )
+
+    return {"access_token": create_access_token(data.email)}
 
 
 @router.get("/me/", response_model=UserDetailResponseSchema)
