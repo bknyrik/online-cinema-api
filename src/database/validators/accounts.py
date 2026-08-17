c55fa7a165378e3f2caee5a7ab15afbdc622ef1f
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-17 08:32:18 +0300

    Fix password validation by digits and letters in validate_passsword

--- src/database/validators/accounts.py
+++ src/database/validators/accounts.py
@@ -14,15 +14,11 @@ def validate_password(password: str) -> str:
     if not re.search(r".{8,16}", password):
         raise ValueError("Password length must be in range 8 and 16")
 
-    elif not re.search(r"\d{3,}", password):
-        raise ValueError(
-            "Password must contain at least 3 digits"
-        )
+    elif not re.search(r"(.*\d.*){3,}", password):
+        raise ValueError("Password must contain at least 3 digits")
 
-    elif not re.search(r"\w{6,}", password):
-        raise ValueError(
-            "Password must contain at least 6 characters"
-        )
+    elif not re.search(r"(.*[a-zA-Z].*){6,}", password):
+        raise ValueError("Password must contain at least 6 letters")
 
     elif not re.search(r"[#!\-_@?.]+", password):
         raise ValueError(
