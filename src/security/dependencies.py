e1cf5aa705c698bdf9d601d38cc6df4ab1e14423
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-17 09:01:40 +0300

    Add dependency get_current_user

---
+++ src/security/dependencies.py
@@ -0,0 +1,32 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security.oauth2 import OAuth2PasswordBearer
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.security.auth import decode_access_token
+from src.database.dependencies import get_db
+from src.database.models.accounts import UserModel
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+async def get_current_user(
+    db: AsyncSession = Depends(get_db),
+    token: str = Depends(oauth2_scheme)
+) -> UserModel:
+    payload = decode_access_token(token)
+
+    result = await db.execute(
+        select(UserModel)
+        .where(UserModel.email == payload["sub"])
+    )
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    return user
