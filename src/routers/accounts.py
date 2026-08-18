00f1829d1e7b07744f3b01f2ddd9bb7ab995efc7
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-18 21:29:47 +0300

    Add handling SQLAlchemyError and add commit and rollback in logout

--- src/routers/accounts.py
+++ src/routers/accounts.py
@@ -240,7 +240,15 @@ async def logout(
     db: AsyncSession = Depends(get_db),
     current_user: UserModel = Depends(get_current_user),
 ) -> None:
-    await delete_token(db, current_user.id, RefreshTokenModel)
+    try:
+        await delete_token(db, current_user.id, RefreshTokenModel)
+        await db.commit()
+    except SQLAlchemyError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while logout"
+        )
 
 
 @router.patch(
