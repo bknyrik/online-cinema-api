e192cf845835e465664088c234b683dac678b81d
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-19 15:07:13 +0300

    Add crud function create_profile

--- src/crud/profiles.py
+++ src/crud/profiles.py
@@ -1,8 +1,11 @@
+import boto3
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 
+from src.settings import settings
 from src.database.models.accounts import UserProfileModel
+from src.schemas.profiles import ProfileFormSchema
 
 
 async def get_profile_by_user_id(
@@ -15,3 +18,16 @@ async def get_profile_by_user_id(
         .where(UserProfileModel.user_id == user_id)
     )
     return result.scalar_one_or_none()
+
+
+async def create_profile(
+    db: AsyncSession,
+    user_id: int,
+    data: dict
+) -> UserProfileModel:
+    profile = UserProfileModel(**data, user_id=user_id)
+
+    db.add(profile)
+    await db.flush()
+
+    return profile
