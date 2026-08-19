19b2ee575842d4b8867a973308b6675f6f35a152
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-19 22:15:46 +0300

    Fix wrong returning url in upload_avatar

--- src/s3/utils.py
+++ src/s3/utils.py
@@ -6,12 +6,14 @@ from src.settings import settings
 
 def upload_avatar(user_id: int, avatar: UploadFile) -> str:
     file_name = f"user_{user_id}.jpg"
-    file_path = f"Avatars/{file_name}"
 
     s3_client.upload_fileobj(
         Fileobj=avatar.file,
         Bucket=settings.AWS_BUCKET_NAME,
-        Key=file_path
+        Key=f"Avatars/{file_name}"
     )
 
-    return f"https://s3://{settings.AWS_BUCKET_NAME}/Avatars/{file_name}"
+    return (
+        f"https://{settings.AWS_BUCKET_NAME}.s3."
+        f"{settings.AWS_REGION_NAME}.amazonaws.com/Avatars/{file_name}"
+    )
