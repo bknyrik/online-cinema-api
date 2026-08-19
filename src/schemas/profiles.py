2b82dcad2a184e38fec6660d1e22cdd206c01802
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-19 11:51:36 +0300

    Add schema ProfileFormSchema

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -1,8 +1,37 @@
 import datetime
 
+from fastapi import Form, UploadFile, File
 from pydantic import BaseModel, HttpUrl
 
 from src.database.models.accounts import GenderEnum
+
+
+class ProfileFormSchema(BaseModel):
+    first_name: str = Form()
+    last_name: str = Form()
+    avatar: UploadFile = File(...)
+    gender: GenderEnum = Form()
+    date_of_birth: datetime.date = Form()
+    info: str = Form()
+
+    @classmethod
+    def as_form(
+        cls,
+        first_name: str = Form(),
+        last_name: str = Form(),
+        avatar: UploadFile = File(...),
+        gender: GenderEnum = Form(),
+        date_of_birth: datetime.date = Form(),
+        info: str = Form()
+    ) -> "ProfileFormSchema":
+        return cls(
+            first_name=first_name,
+            last_name=last_name,
+            avatar=avatar,
+            gender=gender,
+            date_of_birth=date_of_birth,
+            info=info
+        )
 
 
 class ProfileDetailResponseSchema(BaseModel):
