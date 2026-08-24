889d0585c2811f93c668465eae03d0658f4b67dc
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-24 22:38:15 +0300

    Add parameter db to all static methods of CeleryBeatService

--- src/services/celery_beat.py
+++ src/services/celery_beat.py
@@ -1,47 +1,44 @@
 from datetime import datetime
 
 from sqlalchemy import delete
+from sqlalchemy.orm.session import Session
 from sqlalchemy_celery_beat.models import PeriodicTask, ClockedSchedule
-
-from src.database.config import SyncSessionLocal
 
 
 class CeleryBeatService:
 
     @staticmethod
-    def create_clocked_schedule(clocked_time: datetime) -> ClockedSchedule:
-        with SyncSessionLocal() as session:
-            cs = ClockedSchedule(
-                clocked_time=clocked_time
-            )
-            session.add(cs)
-            session.flush()
+    def create_clocked_schedule(
+        db: Session,
+        clocked_time: datetime
+    ) -> ClockedSchedule:
+        cs = ClockedSchedule(clocked_time=clocked_time)
+
+        db.add(cs)
+        db.flush()
 
         return cs
 
     @staticmethod
-    def create_periodic_task(**kwargs) -> PeriodicTask:
-        with SyncSessionLocal() as session:
-            pt = PeriodicTask(**kwargs)
+    def create_periodic_task(db: Session, **kwargs) -> PeriodicTask:
+        pt = PeriodicTask(**kwargs)
 
-            session.add(pt)
-            session.flush()
+        db.add(pt)
+        db.flush()
 
         return pt
 
     @staticmethod
-    def delete_clocked_schedule(id_: int) -> None:
-        with SyncSessionLocal() as session:
-            session.execute(
-                delete(ClockedSchedule)
-                .where(ClockedSchedule.id == id_)
-            )
+    def delete_clocked_schedule(db: Session, id_: int) -> None:
+        db.execute(
+            delete(ClockedSchedule)
+            .where(ClockedSchedule.id == id_)
+        )
 
 
     @staticmethod
-    def delete_periodic_task(id_: int) -> None:
-        with SyncSessionLocal() as session:
-            session.execute(
-                delete(PeriodicTask)
-                .where(PeriodicTask.id == id_)
-            )
+    def delete_periodic_task(db: Session, id_: int) -> None:
+        db.execute(
+            delete(PeriodicTask)
+            .where(PeriodicTask.id == id_)
+        )
