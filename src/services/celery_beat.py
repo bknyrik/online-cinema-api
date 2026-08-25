a78544cd380df0407d5475d01269abd06463556d
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-08-25 07:59:21 +0300

    Add static methods get_clocked_schedule_by_id and get_periodic_task_by_schedule_id

--- src/services/celery_beat.py
+++ src/services/celery_beat.py
@@ -1,11 +1,27 @@
 from datetime import datetime
 
-from sqlalchemy import delete
+from sqlalchemy import select, delete
+from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.session import Session
 from sqlalchemy_celery_beat.models import PeriodicTask, ClockedSchedule
 
 
 class CeleryBeatService:
+
+    @staticmethod
+    def get_clocked_schedule_by_id(db: Session, id_: int) -> ClockedSchedule | None:
+        return db.scalar(
+            select(ClockedSchedule)
+            .where(ClockedSchedule.id == id_)
+        ).one_or_none()
+
+    @staticmethod
+    def get_periodic_task_by_schedule_id(db: Session, schedule_id: int) -> PeriodicTask | None:
+        return db.scalar(
+            select(PeriodicTask)
+            .options(joinedload(PeriodicTask.schedule_model))
+            .where(PeriodicTask.schedule_id == schedule_id)
+        ).one_or_none()
 
     @staticmethod
     def create_clocked_schedule(
