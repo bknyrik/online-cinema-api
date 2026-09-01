8d443df96586ab8a79a92517931d6243698b763d
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-01 13:07:43 +0300

    Fix scalar in get_clocked_schedule_by_id and ArgumentError in get_periodic_task_by_schedule_id

--- src/services/celery_beat.py
+++ src/services/celery_beat.py
@@ -2,7 +2,6 @@ import json
 from datetime import datetime
 
 from sqlalchemy import select, delete
-from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.session import Session
 from sqlalchemy_celery_beat.models import PeriodicTask, ClockedSchedule
 
@@ -34,18 +33,17 @@ class CeleryBeatService:
 
     @staticmethod
     def get_clocked_schedule_by_id(db: Session, id_: int) -> ClockedSchedule | None:
-        return db.scalar(
+        return db.execute(
             select(ClockedSchedule)
             .where(ClockedSchedule.id == id_)
-        ).one_or_none()
+        ).scalar_one_or_none()
 
     @staticmethod
     def get_periodic_task_by_schedule_id(db: Session, schedule_id: int) -> PeriodicTask | None:
-        return db.scalar(
+        return db.execute(
             select(PeriodicTask)
-            .options(joinedload(PeriodicTask.schedule_model))
             .where(PeriodicTask.schedule_id == schedule_id)
-        ).one_or_none()
+        ).scalar_one_or_none()
 
     @staticmethod
     def create_clocked_schedule(
