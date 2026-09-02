8e03e2d11a651aa0499a8d403d4463166c1178f3
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-02 17:54:41 +0300

    Add class repository PeriodicTaskRepository

---
+++ src/repositories/celery_beat.py
@@ -0,0 +1,34 @@
+from src.repositories.base import BaseRepository
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy_celery_beat.models import PeriodicTask
+
+
+class PeriodicTaskRepository(BaseRepository):
+
+    def __init__(self) -> None:
+        super().__init__(PeriodicTask)
+
+    async def aget_by_schedule_id(
+        self,
+        db: AsyncSession,
+        schedule_id: int
+    ) -> PeriodicTask | None:
+        result = await db.execute(
+            select(self._model_type)
+            .where(self._model_type.schedule_id == schedule_id)
+        )
+        return result.scalar_one_or_none()
+
+    def get_by_schedule_id(
+        self,
+        db: Session,
+        schedule_id: int
+    ) -> PeriodicTask | None:
+        result = db.execute(
+            select(self._model_type)
+            .where(self._model_type.schedule_id == schedule_id)
+        )
+        return result.scalar_one_or_none()
