51bb5c6c774fe9e4b9968f9d74a8374e49e0e153
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-02 21:19:59 +0300

    Add logic of PeriodicTaskRepository and ClockedScheduleRepository to delete_expired_activation_token

--- src/celery_beat/tasks.py
+++ src/celery_beat/tasks.py
@@ -2,24 +2,35 @@ from celery import shared_task
 
 from src.database.models.accounts import ActivationTokenModel
 from src.database.config import SyncSessionLocal
-from src.services.accounts import TokenService
-from src.services.celery_beat import CeleryBeatService
+from src.repositories.accounts import TokenRepository
+from src.repositories.celery_beat import (
+    PeriodicTaskRepository,
+    ClockedScheduleRepository
+)
+
 
 @shared_task
 def delete_expired_activation_token(user_id: int, schedule_id: int) -> None:
-    token_service = TokenService(ActivationTokenModel)
+    token_repository = TokenRepository(ActivationTokenModel)
+    ptr = PeriodicTaskRepository()
+    csr = ClockedScheduleRepository()
 
     with SyncSessionLocal() as session:
-        token_service.delete_token(
+        token = token_repository.get_by_user_id(
             db=session,
             user_id=user_id
         )
-        CeleryBeatService.delete_periodic_task_by_schedule_id(
+        token_repository.delete_by_id(db=session, id_=token.id)
+
+        pt = ptr.get_by_schedule_id(db=session, schedule_id=schedule_id)
+        ptr.delete_by_id(
             db=session,
-            schedule_id=schedule_id
+            id_=pt.id
         )
-        CeleryBeatService.delete_clocked_schedule(
+
+        csr.delete_by_id(
             db=session,
             id_=schedule_id
         )
+
         session.commit()
