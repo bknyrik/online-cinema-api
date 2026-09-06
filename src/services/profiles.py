1b4811f77a14ff7aab2ab8fd21067c1c4f3e5981
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-06 17:52:58 +0300

    Add static method create_user_profile to UserProfileService

--- src/services/profiles.py
+++ src/services/profiles.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.models.accounts import UserModel, UserProfileModel
 from src.repositories.profiles import UserProfileRepository
+from src.services.s3 import S3Service
 
 
 class UserProfileService:
@@ -24,5 +25,37 @@ class UserProfileService:
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Profile not found"
             )
+
+        return profile
+
+    @staticmethod
+    async def create_user_profile(
+        db: AsyncSession,
+        s3_service: S3Service,
+        current_user: UserModel,
+        data: dict
+    ) -> UserProfileModel:
+        user_profile_repository = UserProfileRepository()
+
+        profile = await user_profile_repository.aget_by_user_id(
+            db=db,
+            user_id=current_user.id
+        )
+
+        if profile:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="User already has a profile"
+            )
+
+        data["avatar"] = s3_service.upload_image(
+            user_id=current_user.id,
+            image=data["avatar"]
+        )
+
+        profile = await user_profile_repository.acreate(
+            db=db,
+            data=data
+        )
 
         return profile
