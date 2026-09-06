468820acc68798b613350d46b62e3030d8ee67ef
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-06 14:12:30 +0300

    Add call of UserService refresh_access_token inside endpoint refresh_access_token

--- src/routers/accounts.py
+++ src/routers/accounts.py
@@ -264,38 +264,14 @@ async def reset_password_complete(
 )
 async def refresh_access_token(
     data: RefreshAccessTokenRequestSchema,
+    jwt_auth_service: JWTAuthService = Depends(services.get_jwt_auth_service),
     db: AsyncSession = Depends(get_db),
 ) -> dict:
-    user = await get_user_by_email(db, data.email)
-
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found"
-        )
-
-    refresh_token_instance = await get_token_by_user_id(
+    return await UserService.refresh_access_token(
         db=db,
-        user_id=user.id,
-        token_model=RefreshTokenModel
+        data=data.model_dump(),
+        jwt_auth_service=jwt_auth_service
     )
-
-    if (
-        not refresh_token_instance
-        or refresh_token_instance.token != data.refresh_token
-    ):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Token is invalid or missing"
-        )
-
-    if refresh_token_instance.has_expired:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Token is expired"
-        )
-
-    return {"access_token": create_access_token(data.email)}
 
 
 @router.get("/me/", response_model=UserDetailResponseSchema)
