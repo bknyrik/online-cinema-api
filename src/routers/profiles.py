907c2b72735dfe5821a21d43d93426fa3cd009f6
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-06 21:54:55 +0300

    Add endpoint create_current_user_profile

--- src/routers/profiles.py
+++ src/routers/profiles.py
@@ -4,8 +4,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from dependencies.database import get_db
 from src.database.models.accounts import UserModel, UserProfileModel
 from dependencies.authentication import get_current_user
-from src.schemas.profiles import ProfileDetailResponseSchema
+from dependencies.services import get_s3_service
+from src.schemas.profiles import ProfileDetailResponseSchema, ProfileFormSchema
 from src.services.profiles import UserProfileService
+from src.services.s3 import S3Service
 
 
 router = APIRouter()
@@ -18,5 +20,20 @@ async def read_current_user_profile(
 ) -> UserProfileModel:
     return await UserProfileService.read_current_user_profile(
         db=db,
+        current_user=current_user
+    )
+
+
+@router.post("/profiles/", response_model=ProfileDetailResponseSchema)
+async def create_current_user_profile(
+    db: AsyncSession = Depends(get_db),
+    s3_service: S3Service = Depends(get_s3_service),
+    current_user: UserModel = Depends(get_current_user),
+    data: ProfileFormSchema = Depends(ProfileFormSchema.as_form)
+) -> UserProfileModel:
+    return await UserProfileService.create_user_profile(
+        db=db,
+        data=data.model_dump(),
+        s3_service=s3_service,
         current_user=current_user
     )
