5c9e136bc8afff12353879bb2c0bc1f87f5fca59
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-06 10:00:13 +0300

    Add logic of UserRepository and JWTAuthService to get_current_admin_user

--- src/dependencies/authentication.py
+++ src/dependencies/authentication.py
@@ -1,10 +1,7 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security.oauth2 import OAuth2PasswordBearer
-from sqlalchemy import select
-from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.security.auth import decode_access_token
 from dependencies.database import get_db
 from src.database.models.accounts import UserModel
 from src.repositories.accounts import UserRepository
@@ -38,12 +35,28 @@ async def get_current_user(
 
 
 async def get_current_admin_user(
-    current_user: UserModel = Depends(get_current_user)
+    db: AsyncSession = Depends(get_db),
+    token: str = Depends(oauth2_scheme),
+    jwt_auth_service: JWTAuthService = Depends(get_jwt_auth_service)
 ) -> UserModel:
-    if current_user.group.name.value != "admin":
+    user_repository = UserRepository()
+    payload = jwt_auth_service.decode_token(token)
+
+    user = await user_repository.aget_with_group_by_id(
+        db=db,
+        id_=int(payload["sub"])
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    if user.group.name.value != "admin":
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="User is not an admin to perform this action"
         )
 
-    return current_user
+    return user
