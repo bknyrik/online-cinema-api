e31043e6e890d03e7ae647e1ef3dccada8418e0c
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-06 17:15:47 +0300

    Add method aget_by_user_id to UserProfileRepository

--- src/repositories/profiles.py
+++ src/repositories/profiles.py
@@ -1,5 +1,7 @@
-from src.repositories.base import BaseRepository
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
+from src.repositories.base import BaseRepository
 from src.database.models.accounts import UserProfileModel
 
 
@@ -7,3 +9,14 @@ class UserProfileRepository(BaseRepository[UserProfileModel]):
 
     def __init__(self) -> None:
         super().__init__(UserProfileModel)
+
+    async def aget_by_user_id(
+        self,
+        db: AsyncSession,
+        user_id: int
+    ) -> UserProfileModel | None:
+        result = await db.execute(
+            select(self._model_type)
+            .where(self._model_type.user_id == user_id)
+        )
+        return result.scalar_one_or_none()
