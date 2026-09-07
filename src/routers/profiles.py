8a590e1b7fff9248ff631ec96fb90053a501a057
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-07 13:17:14 +0300

    Add endpoint update_current_user_profile

--- src/routers/profiles.py
+++ src/routers/profiles.py
@@ -6,7 +6,11 @@ from services.profiles import UserProfileService
 from src.database.models.accounts import UserModel, UserProfileModel
 from dependencies.authentication import get_current_user
 from dependencies.services import get_s3_service
-from src.schemas.profiles import ProfileDetailResponseSchema, ProfileFormSchema
+from src.schemas.profiles import (
+    ProfileDetailResponseSchema,
+    ProfileFormSchema,
+    ProfileUpdateFormSchema
+)
 from src.services.profiles import UserProfileService
 from src.services.s3 import S3Service
 
@@ -37,6 +41,21 @@ async def create_current_user_profile(
         data=data.model_dump(),
         s3_service=s3_service,
         current_user=current_user
+    )
+
+
+@router.patch("/me/", response_model=ProfileDetailResponseSchema)
+async def update_current_user_profile(
+    db: AsyncSession = Depends(get_db),
+    s3_service: S3Service = Depends(get_s3_service),
+    data: ProfileUpdateFormSchema = Depends(ProfileUpdateFormSchema.as_form),
+    current_user: UserModel = Depends(get_current_user)
+) -> UserProfileModel:
+    return await UserProfileService.update_current_user_profile(
+        db=db,
+        data=data.model_dump(),
+        current_user=current_user,
+        s3_service=s3_service
     )
 
 
