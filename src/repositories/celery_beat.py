a3ab261240e3092ede7030aeabb420b69aeca759
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-08 18:12:13 +0300

    Remove aget_by_schedule_id of PeriodicTaskRepository

--- src/repositories/celery_beat.py
+++ src/repositories/celery_beat.py
@@ -11,17 +11,6 @@ class PeriodicTaskRepository(BaseRepository):
     def __init__(self) -> None:
         super().__init__(PeriodicTask)
 
-    async def aget_by_schedule_id(
-        self,
-        db: AsyncSession,
-        schedule_id: int
-    ) -> PeriodicTask | None:
-        result = await db.execute(
-            select(self._model_type)
-            .where(self._model_type.schedule_id == schedule_id)
-        )
-        return result.scalar_one_or_none()
-
     def get_by_schedule_id(
         self,
         db: Session,
