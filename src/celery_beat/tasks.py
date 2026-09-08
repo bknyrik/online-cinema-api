19aa21abbafc1d8e27e6503c6b6eba4fec3546c9
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-08 18:11:42 +0300

    Add call of TokenRepository delete_by_user_id in delete_expired_activation_token

--- src/celery_beat/tasks.py
+++ src/celery_beat/tasks.py
@@ -16,11 +16,10 @@ def delete_expired_activation_token(user_id: int, schedule_id: int) -> None:
     csr = ClockedScheduleRepository()
 
     with SyncSessionLocal() as session:
-        token = token_repository.get_by_user_id(
+        token_repository.delete_by_user_id(
             db=session,
             user_id=user_id
         )
-        token_repository.delete_by_id(db=session, id_=token.id)
 
         pt = ptr.get_by_schedule_id(db=session, schedule_id=schedule_id)
         ptr.delete_by_id(
