12e8253fba8c8dccdae5c3e6857971896b947acf
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-09 21:13:10 +0300

    Add class repository GenreRepository

---
+++ src/repositories/movies.py
@@ -0,0 +1,9 @@
+from src.repositories.base import AsyncBaseRepository
+
+from src.database.models import movies
+
+
+class GenreRepository(AsyncBaseRepository[movies.Genre]):
+
+    def __init__(self) -> None:
+        super().__init__(movies.Genre)
