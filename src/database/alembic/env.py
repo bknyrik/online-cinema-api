59444e784868c0ee8c0e06eb3b067aee4f424e2f
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-09 21:06:11 +0300

    Make migrations Create tables associated with movies

--- src/database/alembic/env.py
+++ src/database/alembic/env.py
@@ -9,6 +9,8 @@ from alembic import context
 
 from src.database.config import SQLALCHEMY_DATABASE_URL
 from src.database.models.accounts import *
+from src.database.models.movies import *
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
