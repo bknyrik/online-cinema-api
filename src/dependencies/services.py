0f209965d403e99fc2559e9989ffd58ab4d807bb
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-09 10:32:10 +0300

    Add dependency get_user_service

--- src/dependencies/services.py
+++ src/dependencies/services.py
@@ -1,9 +1,16 @@
 from datetime import timedelta
 
 from src.settings import settings
+from src.services.accounts import UserService
 from src.services.security import PasswordSecurityService, JWTAuthService
 from src.services.email_sender import EmailSenderService
 from src.services.s3 import S3Service
+from src.repositories.accounts import (
+    UserRepository,
+    TokenRepository,
+    UserGroupRepository
+)
+from src.database.models import accounts
 
 
 ACCESS_TOKEN_LIFETIME = timedelta(minutes=30)
@@ -36,4 +43,14 @@ def get_s3_service() -> S3Service:
         secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
         bucket_name=settings.AWS_BUCKET_NAME,
         region_name=settings.AWS_REGION_NAME
+    )
+
+
+def get_user_service() -> UserService:
+    return UserService(
+        user_repository=UserRepository(),
+        user_group_repository=UserGroupRepository(),
+        at_repository=TokenRepository(accounts.ActivationTokenModel),
+        rt_repository=TokenRepository(accounts.RefreshTokenModel),
+        prt_repository=TokenRepository(accounts.PasswordResetTokenModel)
     )
