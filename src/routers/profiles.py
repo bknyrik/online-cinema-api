97bc0ef6072943a52d197fd4226020da063d478e
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-09 11:54:58 +0300

    Add UserProfileService as dependency to all profiles endpoints

--- src/routers/profiles.py
+++ src/routers/profiles.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from dependencies.database import get_db
 from src.database.models.accounts import UserModel, UserProfileModel
 from dependencies.authentication import get_current_user
-from dependencies.services import get_s3_service
+from dependencies.services import get_s3_service, get_profile_service
 from src.schemas.profiles import (
     ProfileDetailResponseSchema,
     ProfileFormSchema,
@@ -20,9 +20,10 @@ router = APIRouter()
 @router.get("/me/", response_model=ProfileDetailResponseSchema)
 async def read_current_user_profile(
     db: AsyncSession = Depends(get_db),
+    user_profile_service: UserProfileService = Depends(get_profile_service),
     current_user: UserModel = Depends(get_current_user)
 ) -> UserProfileModel:
-    return await UserProfileService.read_current_user_profile(
+    return await user_profile_service.read_current_user_profile(
         db=db,
         current_user=current_user
     )
@@ -31,11 +32,12 @@ async def read_current_user_profile(
 @router.post("/", response_model=ProfileDetailResponseSchema)
 async def create_current_user_profile(
     db: AsyncSession = Depends(get_db),
+    user_profile_service: UserProfileService = Depends(get_profile_service),
     s3_service: S3Service = Depends(get_s3_service),
     current_user: UserModel = Depends(get_current_user),
     data: ProfileFormSchema = Depends(ProfileFormSchema.as_form)
 ) -> UserProfileModel:
-    return await UserProfileService.create_user_profile(
+    return await user_profile_service.create_user_profile(
         db=db,
         data=data.model_dump(),
         s3_service=s3_service,
@@ -46,11 +48,12 @@ async def create_current_user_profile(
 @router.patch("/me/", response_model=ProfileDetailResponseSchema)
 async def update_current_user_profile(
     db: AsyncSession = Depends(get_db),
+    user_profile_service: UserProfileService = Depends(get_profile_service),
     s3_service: S3Service = Depends(get_s3_service),
     data: ProfileUpdateFormSchema = Depends(ProfileUpdateFormSchema.as_form),
     current_user: UserModel = Depends(get_current_user)
 ) -> UserProfileModel:
-    return await UserProfileService.update_current_user_profile(
+    return await user_profile_service.update_current_user_profile(
         db=db,
         data=data.model_dump(),
         current_user=current_user,
@@ -61,9 +64,10 @@ async def update_current_user_profile(
 @router.delete("/me/")
 async def delete_current_user_profile(
     db: AsyncSession = Depends(get_db),
+    user_profile_service: UserProfileService = Depends(get_profile_service),
     current_user: UserModel = Depends(get_current_user)
 ) -> None:
-    return await UserProfileService.delete_current_user_profile(
+    return await user_profile_service.delete_current_user_profile(
         db=db,
         current_user=current_user
     )
