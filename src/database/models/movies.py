9027199ced83dbf63ee74063dfd2a2b8846352b7
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-09 20:33:21 +0300

    Add relationship movies to Genre

--- src/database/models/movies.py
+++ src/database/models/movies.py
@@ -43,6 +43,10 @@ class Genre(Base):
 
     id = Column(Integer, nullable=False, primary_key=True, index=True)
     name = Column(String(64), nullable=False, unique=True)
+    movies = relationship(
+        secondary=MovieGenresModel,
+        back_populates="genres"
+    )
 
 
 class Star(Base):
