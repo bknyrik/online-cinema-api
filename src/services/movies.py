472eba51f5ce8ba91f214b69ea35facfa5c1104a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-10 06:43:34 +0300

    Add class service MovieService

---
+++ src/services/movies.py
@@ -0,0 +1,29 @@
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
+
+from src.database.models.movies import MovieModel
+from src.repositories import movies
+
+
+class MovieService:
+
+    def __init__(self, movie_repository: movies.MovieRepository) -> None:
+        self.movie_repository = movie_repository
+
+    async def get_detail_movie(self, db: AsyncSession, movie_id: int) -> MovieModel:
+        try:
+            movie = await self.movie_repository.aget_by_id(db, movie_id)
+
+            if not movie:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Movie not found"
+                )
+
+            return movie
+        except SQLAlchemyError:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="An error occurred while getting movie"
+            )
