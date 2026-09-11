e9485772ff64d327ae0eb7495dfdfe63ad80bce2
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-11 19:36:59 +0300

    Rename return dict key and query parameter imdb_min_rate on min_imdb of movie_filter_parameters

--- src/dependencies/movies.py
+++ src/dependencies/movies.py
@@ -8,7 +8,7 @@ async def movie_filter_parameters(
     max_year: int = Query(default=None),
     min_time: int = Query(default=None),
     max_time: int = Query(default=None),
-    imdb_min_rate: int = Query(default=None),
+    min_imdb: int = Query(default=None),
     imdb_max_rate: int = Query(default=None),
     min_price: int = Query(default=None),
     max_price: int = Query(default=None)
@@ -18,7 +18,7 @@ async def movie_filter_parameters(
         "max_year": max_year,
         "min_time": min_time,
         "max_time": max_time,
-        "imdb_min_rate": imdb_min_rate,
+        "min_imdb": min_imdb,
         "imdb_max_rate": imdb_max_rate,
         "min_price": min_price,
         "max_price": max_price
