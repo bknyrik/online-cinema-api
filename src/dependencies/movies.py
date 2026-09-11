2818cc609a17c090616baf4332a4bbcc4e39333e
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-11 12:42:50 +0300

    Add dependency movie_filter_parameters and annotated type MovieFilterDep

---
+++ src/dependencies/movies.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+
+from fastapi import Depends, Query
+
+
+async def movie_filter_parameters(
+    min_release_year: int = Query(default=None),
+    max_release_year: int = Query(default=None),
+    min_time: int = Query(default=None),
+    max_time: int = Query(default=None),
+    imdb_min_rate: int = Query(default=None),
+    imdb_max_rate: int = Query(default=None),
+    min_price: int = Query(default=None),
+    max_price: int = Query(default=None)
+) -> dict:
+    return {
+        "min_release_year": min_release_year,
+        "max_release_year": max_release_year,
+        "min_time": min_time,
+        "max_time": max_time,
+        "imdb_min_rate": imdb_min_rate,
+        "imdb_max_rate": imdb_max_rate,
+        "min_price": min_price,
+        "max_price": max_price
+    }
+
+
+MovieFilterDep = Annotated[dict, Depends(movie_filter_parameters)]
