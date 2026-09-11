501924244a07a74b9589b3f6dafbcc7236615651
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-11 19:19:19 +0300

    Rename return dict key and query parameter max_release_year on max_year of movie_filter_parameters

--- src/dependencies/movies.py
+++ src/dependencies/movies.py
@@ -5,7 +5,7 @@ from fastapi import Depends, Query
 
 async def movie_filter_parameters(
     min_year: int = Query(default=None),
-    max_release_year: int = Query(default=None),
+    max_year: int = Query(default=None),
     min_time: int = Query(default=None),
     max_time: int = Query(default=None),
     imdb_min_rate: int = Query(default=None),
@@ -15,7 +15,7 @@ async def movie_filter_parameters(
 ) -> dict:
     return {
         "min_year": min_year,
-        "max_release_year": max_release_year,
+        "max_year": max_year,
         "min_time": min_time,
         "max_time": max_time,
         "imdb_min_rate": imdb_min_rate,
