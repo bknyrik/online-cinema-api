9cf18d08a2bd8b06d94549262fcffecb0374c583
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-13 13:00:15 +0300

    Fix wrong returning value to get_genre_list

--- src/services/genres.py
+++ src/services/genres.py
@@ -2,7 +2,6 @@ from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
 
-from src.database.models.movies import GenreModel
 from src.repositories.movies import GenreRepository
 
 
@@ -11,10 +10,12 @@ class GenreService:
     def __init__(self, genre_repository: GenreRepository) -> None:
         self.genre_repository = genre_repository
 
-    async def get_genre_list(self, db: AsyncSession) -> list[GenreModel]:
+    async def get_genre_list(self, db: AsyncSession) -> dict:
         try:
             genres = list(await self.genre_repository.aget_all(db))
-            return genres
+            return {
+                "genres": genres
+            }
         except SQLAlchemyError:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
