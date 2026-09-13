b2cc359141c6828d34fce68363c32edfb825d1e0
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-13 13:49:19 +0300

    Add pagination_data as dependency to get_genre_list

--- src/routers/genres.py
+++ src/routers/genres.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.dependencies.database import get_db
 from src.services.genres import GenreService
 from src.dependencies.services import get_genre_service
+from src.dependencies.pagination import PaginationDep
 from src.schemas.genres import GenreListResponseSchema
 
 
@@ -12,6 +13,7 @@ router = APIRouter()
 
 @router.get("/", response_model=GenreListResponseSchema)
 async def get_genre_list(
+    pagination_data: PaginationDep,
     db: AsyncSession = Depends(get_db),
     genre_service: GenreService = Depends(get_genre_service)
 ) -> dict:
