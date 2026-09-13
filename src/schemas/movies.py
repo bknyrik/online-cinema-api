0befe708ddb197410eb6657a7aa4db5082f894f1
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-13 10:34:16 +0300

    Change field type on StrictInt to field year and time of MovieDataRequestBaseSchema

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -8,7 +8,8 @@ from pydantic import (
     Field
 )
 from pydantic.types import (
-    StrictFloat
+    StrictFloat,
+    StrictInt
 )
 
 
@@ -72,8 +73,8 @@ class MovieDetailResponseSchema(MovieDetailBaseSchema):
 
 class MovieDataRequestBaseSchema(BaseModel):
     name: str = Field(min_length=3)
-    year: int = Field(ge=1)
-    time: int = Field(ge=1)
+    year: StrictInt = Field(ge=1)
+    time: StrictInt = Field(ge=1)
     imdb: StrictFloat = Field(ge=1, max_digits=1, le=10)
     votes: int = Field(ge=1)
     meta_score: float = Field(ge=1)
