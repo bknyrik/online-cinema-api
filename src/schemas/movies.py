8b131b203b25a86eb516e5130334710d0b142844
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-13 10:40:01 +0300

    Add validation option le to field meta_score of MovieDataRequestBaseSchema

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -77,7 +77,7 @@ class MovieDataRequestBaseSchema(BaseModel):
     time: StrictInt = Field(ge=1)
     imdb: StrictFloat = Field(ge=1, max_digits=1, le=10)
     votes: StrictInt = Field(ge=1)
-    meta_score: StrictFloat = Field(ge=1)
+    meta_score: StrictFloat = Field(ge=1, le=100)
     gross: StrictFloat = Field(ge=1)
     description: str = Field(min_length=10)
     price: Decimal = Field(ge=1)
