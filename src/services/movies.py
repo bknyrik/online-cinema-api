09bc7e703b7c9ef68ee742e6f07c41046e265e65
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-13 12:28:45 +0300

    Refactor value for key of return dictionary in get_movie_list of MovieService

--- src/services/movies.py
+++ src/services/movies.py
@@ -156,26 +156,7 @@ class MovieService:
                 if page < total_pages else None
             )
             return {
-                "movies": [
-                    {
-                        "id": movie.id,
-                        "uuid": movie.uuid,
-                        "name": movie.name,
-                        "year": movie.year,
-                        "time": movie.time,
-                        "imdb": movie.imdb,
-                        "votes": movie.votes,
-                        "meta_score": movie.meta_score,
-                        "gross": movie.gross,
-                        "description": movie.description,
-                        "price": movie.price,
-                        "certification": movie.certification.name,
-                        "genres": [genre.name for genre in movie.genres],
-                        "stars": [star.name for star in movie.stars],
-                        "directors": [director.name for director in movie.directors],
-                    }
-                    for movie in movies_list
-                ],
+                "movies": movies_list,
                 "total_movies": total_movies,
                 "total_pages": total_pages,
                 "prev": prev,
