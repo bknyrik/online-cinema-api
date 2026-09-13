7cf83abc1849ce2b3ba7d332ed130e0d23232f23
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-13 13:02:17 +0300

    Add endpoint get_genre_list

--- src/routers/genres.py
+++ src/routers/genres.py
@@ -1,4 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.dependencies.database import get_db
+from src.services.genres import GenreService
+from src.dependencies.services import get_genre_service
+from src.schemas.genres import GenreListResponseSchema
 
 
 router = APIRouter()
+
+
+@router.get("/", response_model=GenreListResponseSchema)
+async def get_genre_list(
+    db: AsyncSession = Depends(get_db),
+    genre_service: GenreService = Depends(get_genre_service)
+) -> dict:
+    return await genre_service.get_genre_list(db)
