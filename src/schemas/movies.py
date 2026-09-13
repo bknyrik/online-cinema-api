513f19bcc571fbdb2e98b378258989d12905344a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-13 11:05:55 +0300

    Change base class on MovieDataRequestBaseSchema to MovieUpdateRequestSchema

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -99,17 +99,22 @@ class MovieDataRequestBaseSchema(BaseModel):
 class MovieCreateRequestSchema(MovieDataRequestBaseSchema):
     ...
 
-class MovieUpdateRequestSchema(BaseModel):
-    name: str | None = None
-    year: int | None = None
-    time: int | None = None
-    imdb: float | None = None
-    votes: int | None = None
-    meta_score: float | None = None
-    gross: float | None = None
-    description: str | None = None
-    price: Decimal | None = None
-    certification: int | None = None
-    genres: list[int] | None = None
-    stars: list[int] | None = None
-    directors: list[int] | None = None
+class MovieUpdateRequestSchema(MovieDataRequestBaseSchema):
+    name: StrictStr | None = Field(min_length=3, default=None)
+    year: StrictInt | None = Field(ge=1, default=None)
+    time: StrictInt | None = Field(ge=1, default=None)
+    imdb: StrictFloat | None = Field(ge=1, le=10, default=None)
+    votes: StrictInt| None = Field(ge=1, default=None)
+    meta_score: StrictFloat | None = Field(ge=1, le=100, default=None)
+    gross: StrictFloat | None = Field(ge=1, default=None)
+    description: StrictStr | None = Field(min_length=10, default=None)
+    price: Decimal | None = Field(
+        ge=1,
+        max_digits=10,
+        decimal_places=2,
+        default=None
+    )
+    certification: StrictInt | None = Field(default=None)
+    genres: list[StrictInt] | None = Field(default=None)
+    stars: list[StrictInt] | None = Field(default=None)
+    directors: list[StrictInt] | None = Field(default=None)
