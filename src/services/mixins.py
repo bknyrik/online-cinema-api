db7729019c57dd5465d4b57e64540855369f8e7a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-15 12:11:12 +0300

    Add static method validate_page_not_found to PaginationLimitOffsetMixin

--- src/services/mixins.py
+++ src/services/mixins.py
@@ -1,5 +1,7 @@
 import math
 
+from fastapi import HTTPException, status
+
 
 class PaginationLimitOffsetMixin:
 
@@ -13,3 +15,15 @@ class PaginationLimitOffsetMixin:
     @staticmethod
     def get_total_pages(total_items: int, per_page: int) -> int:
         return math.ceil(total_items / per_page)
+
+    @staticmethod
+    def validate_page_not_found(
+        page: int,
+        total_pages: int,
+        total_items: int
+    ) -> None:
+        if page > total_pages and total_items:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Page not found"
+            )
