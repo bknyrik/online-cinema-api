70ed82419d6098f319a7de26c4c1b237c52754d9
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-15 16:32:56 +0300

    Add static method get_prev_next_urls_pages to PaginationLimitOffsetMixin

--- src/services/mixins.py
+++ src/services/mixins.py
@@ -1,4 +1,5 @@
 import math
+from urllib import parse
 
 from fastapi import HTTPException, status
 
@@ -15,6 +16,20 @@ class PaginationLimitOffsetMixin:
     @staticmethod
     def get_total_pages(total_items: int, per_page: int) -> int:
         return math.ceil(total_items / per_page)
+
+    @staticmethod
+    def get_prev_next_urls_pages(
+        url: str,
+        page: int,
+        total_pages: int,
+        query_params: dict
+    ) -> tuple[str | None, str | None]:
+        prev_page = url + parse.urlencode(query_params) if page > 1 else None
+        next_page = (
+            url + parse.urlencode(query_params)
+            if page < total_pages else None
+        )
+        return prev_page, next_page
 
     @staticmethod
     def validate_page_not_found(
