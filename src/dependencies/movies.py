e7db890169b4aeb80c774dd8ebac477288a75d60
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-15 14:09:38 +0300

    Remov suffix _ids to key genres of return dictionary of movie_filter_params

--- src/dependencies/movies.py
+++ src/dependencies/movies.py
@@ -23,7 +23,7 @@ async def movie_filter_params(
 ) -> dict:
     return {
         "certification": filter_by_certification_id,
-        "genres_ids": filter_by_genres_ids,
+        "genres": filter_by_genres_ids,
         "min_year": filter_by_min_year,
         "max_year": filter_by_max_year,
         "min_time": filter_by_min_time,
