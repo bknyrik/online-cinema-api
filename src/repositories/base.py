c29cfebdcb965afffd85d9e3c3c61032cce26750
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-17 20:54:34 +0300

    Add parameter select_columns to get records with selected columns to aget_all of AsyncBaseRepository

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -20,13 +20,17 @@ class AsyncBaseRepository[T]:
     async def aget_all(
         self,
         db: AsyncSession,
+        select_columns: list | None = None,
         offset: int | None = None,
         limit: int | None = None,
         join_relationships: list[str] | None = None,
         expressions: list[ColumnElement[bool]] | None = None,
         order_by_columns: list[ColumnElement[T]] | None = None
     ) -> Sequence[T]:
-        stmt = select(self._model_type)
+        stmt = select(
+            self._model_type if select_columns is None
+            else select_columns
+        )
 
         if join_relationships:
             for relationship in join_relationships:
