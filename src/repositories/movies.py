aa893e9d7406dee4f88f858dca6166c94e677cf2
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-18 16:05:51 +0300

    Add method acount_movies to GenreRepository

--- src/repositories/movies.py
+++ src/repositories/movies.py
@@ -1,3 +1,8 @@
+from typing import Sequence
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func
+
 from src.repositories.base import AsyncBaseRepository
 from src.database.models import movies
 
@@ -6,6 +11,19 @@ class GenreRepository(AsyncBaseRepository[movies.GenreModel]):
 
     def __init__(self) -> None:
         super().__init__(movies.GenreModel)
+
+    async def acount_movies(self, db: AsyncSession) -> Sequence[int]:
+        result = await db.execute(
+            select(func.count(movies.MoviesGenresModel.c.movie_id))
+            .join_from(
+                self._model_type,
+                movies.MoviesGenresModel,
+                isouter=True
+            )
+            .group_by(self._model_type.id)
+            .order_by(self._model_type.id)
+        )
+        return result.scalars().all()
 
 
 class StarRepository(AsyncBaseRepository[movies.StarModel]):
