7697df3ae2adc6e84dc2792c6facc8ed9d025764
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-18 16:37:49 +0300

    Add endpoint get_genre_detail

--- src/routers/genres.py
+++ src/routers/genres.py
@@ -5,7 +5,11 @@ from src.dependencies.database import get_db
 from src.services.genres import GenreService
 from src.dependencies.services import get_genre_service
 from src.dependencies.pagination import PaginationDep
-from src.schemas.genres import GenreListResponseSchema
+from src.schemas.genres import (
+    GenreListResponseSchema,
+    GenreDetailResponseSchema
+)
+from src.database.models.movies import GenreModel
 
 
 router = APIRouter()
@@ -20,4 +24,16 @@ async def get_genre_list(
     return await genre_service.get_genre_list(
         db=db,
         pagination_data=pagination_data
+    )
+
+
+@router.get("/{genre_id}/", response_model=GenreDetailResponseSchema)
+async def get_genre_detail(
+    genre_id: int,
+    db: AsyncSession = Depends(get_db),
+    genre_service: GenreService = Depends(get_genre_service)
+) -> GenreModel:
+    return await genre_service.get_genre_detail(
+        db=db,
+        genre_id=genre_id
     )
