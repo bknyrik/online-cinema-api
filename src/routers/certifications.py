99130a437a9fa4a7d570481f9456d991433e07ae
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-19 22:10:06 +0300

    Change response_model on CertificationBaseSchema to create_certification

--- src/routers/certifications.py
+++ src/routers/certifications.py
@@ -47,7 +47,7 @@ async def get_certification_detail(
 @router.post(
     "/",
     status_code=status.HTTP_201_CREATED,
-    response_model=certifications.CertificationDataResponseSchema
+    response_model=certifications.CertificationBaseSchema
 )
 async def create_certification(
     data: certifications.CertificationCreateRequestSchema,
