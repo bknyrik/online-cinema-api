c17e81df2d70079f677f5eef0ef7efbc6a274671
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-19 18:40:03 +0300

    Add endpoint get_star_list

--- src/routers/stars.py
+++ src/routers/stars.py
@@ -1,4 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.dependencies.database import get_db
+from src.dependencies.pagination import PaginationDep
+from src.schemas import stars as stars_schemas
+from src.dependencies.services import get_star_service
+from src.services.stars import StarService
 
 
 router = APIRouter()
+
+
+@router.get(
+    "/",
+    response_model=stars_schemas.StarListResponseSchema
+)
+async def get_star_list(
+    pagination_data: PaginationDep,
+    db: AsyncSession = Depends(get_db),
+    star_service: StarService = Depends(get_star_service)
+) -> dict:
+    return await star_service.get_star_list(
+        pagination_data=pagination_data,
+        db=db
+    )
