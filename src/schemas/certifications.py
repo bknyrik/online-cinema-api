5753ed66fdf9625efa711988bb8ed497b88e0f3a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-19 13:10:49 +0300

    Add schemas CertificationCreateRequestSchema and CertificationUpdateRequestSchema

--- src/schemas/certifications.py
+++ src/schemas/certifications.py
@@ -22,3 +22,11 @@ class CertificationListResponseSchema(BaseModel):
 
 class CertificationDataRequestSchema(BaseModel):
     name: str = Field(min_length=1, strict=True)
+
+
+class CertificationCreateRequestSchema(CertificationDataRequestSchema):
+    ...
+
+
+class CertificationUpdateRequestSchema(CertificationDataRequestSchema):
+    ...
