891e0dbee8a1d62e6bdda50f884134e7e2e7802b
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-19 12:57:01 +0300

    Add endpoint delete_certification

--- src/routers/certifications.py
+++ src/routers/certifications.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.dependencies.database import get_db
+from src.dependencies.authentication import get_current_moderator_or_admin
 from src.dependencies.pagination import PaginationDep
 from src.dependencies.services import get_certification_service
 from src.services.certifications import CertificationService
 from src.schemas import certifications
 from src.database.models.movies import CertificationModel
+from src.database.models.accounts import UserModel
 
 
 router = APIRouter()
@@ -72,4 +74,20 @@ async def update_certification(
         db=db,
         certification_id=certification_id,
         data=data.model_dump()
+    )
+
+
+@router.delete(
+    "/{certification_id}/",
+    status_code=status.HTTP_204_NO_CONTENT
+)
+async def delete_certification(
+    certification_id: int,
+    db: AsyncSession,
+    certification_service: CertificationService = Depends(get_certification_service),
+    current_user: UserModel = Depends(get_current_moderator_or_admin)
+) -> None:
+    return await certification_service.delete_certification(
+        db=db,
+        certification_id=certification_id
     )
