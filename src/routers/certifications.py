fde064cd868d7a835353a9f64954af9fd1bb24fa
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-19 11:51:33 +0300

    Add endpoint get_certification_detail

--- src/routers/certifications.py
+++ src/routers/certifications.py
@@ -6,6 +6,7 @@ from src.dependencies.pagination import PaginationDep
 from src.dependencies.services import get_certification_service
 from src.services.certifications import CertificationService
 from src.schemas import certifications
+from src.database.models.movies import CertificationModel
 
 
 router = APIRouter()
@@ -23,4 +24,19 @@ async def get_certification_list(
     return await certification_service.get_certification_list(
         pagination_data=pagination_data,
         db=db
+    )
+
+
+@router.get(
+    "/{certification_id}/",
+    response_model=certifications.CertificationDetailBaseSchema
+)
+async def get_certification_detail(
+    certification_id: int,
+    db: AsyncSession = Depends(get_db),
+    certification_service: CertificationService = Depends(get_certification_service)
+) -> CertificationModel:
+    return await certification_service.get_certification_detail(
+        db=db,
+        certification_id=certification_id
     )
