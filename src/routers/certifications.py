d1d191e14707fbf2630dc07f612b4263f64375d2
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-19 11:45:46 +0300

    Add endpoint get_certification_list

--- src/routers/certifications.py
+++ src/routers/certifications.py
@@ -1,4 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.dependencies.database import get_db
+from src.dependencies.pagination import PaginationDep
+from src.dependencies.services import get_certification_service
+from src.services.certifications import CertificationService
+from src.schemas import certifications
 
 
 router = APIRouter()
+
+
+@router.get(
+    "/",
+    response_model=certifications.CertificationListResponseSchema
+)
+async def get_certification_list(
+    pagination_data: PaginationDep,
+    db: AsyncSession = Depends(get_db),
+    certification_service: CertificationService = Depends(get_certification_service),
+) -> dict:
+    return await certification_service.get_certification_list(
+        pagination_data=pagination_data,
+        db=db
+    )
