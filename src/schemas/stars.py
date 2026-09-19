7b2ccb22b9d55cb835b5fb2a68428e6a46fedb1d
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-19 18:03:19 +0300

    Add schema StarListResponseSchema

--- src/schemas/stars.py
+++ src/schemas/stars.py
@@ -4,3 +4,11 @@ from pydantic import BaseModel
 class StarBaseSchema(BaseModel):
     id: int
     name: str
+
+
+class StarListResponseSchema(BaseModel):
+    stars: list[StarBaseSchema]
+    total_stars: int
+    total_pages: int
+    prev: str | None
+    next: str | None
