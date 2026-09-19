df666b16dcd6e3591d313a79179da3c71b16afab
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-19 07:54:34 +0300

    Add schema CertificationListResponseSchema

--- src/schemas/certification.py
+++ src/schemas/certification.py
@@ -1,6 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class CertificationDetailBaseSchema(BaseModel):
     id: int
     name: str
+
+
+class CertificationListResponseSchema(BaseModel):
+    certifications: list[CertificationDetailBaseSchema]
+    total_certifications: int
+    total_pages: int
+    prev: str | None
+    next: str | None
+
+    model_config = ConfigDict(from_attributes=True)
