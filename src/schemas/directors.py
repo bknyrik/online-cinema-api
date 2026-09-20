1355bb5a0fa67d221f64aac62a222c2e11670e86
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-20 09:35:35 +0300

    Add schema DirectorListResponseSchema

--- src/schemas/directors.py
+++ src/schemas/directors.py
@@ -4,3 +4,11 @@ from pydantic import BaseModel
 class DirectorDetailResponseSchema(BaseModel):
     id: int
     name: str
+
+
+class DirectorListResponseSchema(BaseModel):
+    directors: list[DirectorDetailResponseSchema]
+    total_directors: int
+    total_pages: int
+    prev: str | None
+    next: str | None
