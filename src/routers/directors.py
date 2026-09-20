7b11cb1233553aa4f26a9eb4321bcf5e9d23632d
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-20 09:56:55 +0300

    Add endpoint get_director_list

--- src/routers/directors.py
+++ src/routers/directors.py
@@ -1,4 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.dependencies.database import get_db
+from src.services.directors import DirectorService
+from src.schemas import directors as schemas
+from src.dependencies.services import get_director_service
+from src.dependencies.pagination import PaginationDep
 
 
 router = APIRouter()
+
+
+@router.get("/", response_model=schemas.DirectorListResponseSchema)
+async def get_director_list(
+    pagination_data: PaginationDep,
+    db: AsyncSession = Depends(get_db),
+    director_service: DirectorService = Depends(get_director_service)
+) -> dict:
+    return await director_service.get_director_list(
+        db=db,
+        pagination_data=pagination_data
+    )
