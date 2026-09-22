558622bdc8459d63d1a9f2e5433c73a052e9aeda
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-22 17:08:05 +0300

    Add repository LikeMovieRepository

---
+++ src/repositories/rating.py
@@ -0,0 +1,9 @@
+from src.repositories.base import AsyncBaseRepository
+
+from src.database.models import rating
+
+
+class LikeMovieRepository(AsyncBaseRepository):
+
+    def __init__(self) -> None:
+        super().__init__(rating.LikeMovieModel)
