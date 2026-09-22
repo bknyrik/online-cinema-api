c6466bd75fb02668529b7f9cb5c28be49ed4bf4a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-22 17:05:41 +0300

    Add schema LikeMovieListResponseSchema

--- src/schemas/rating.py
+++ src/schemas/rating.py
@@ -1,6 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class LikeMovieListItemSchema(BaseModel):
     id: int
     movie_id: int
+
+
+class LikeMovieListResponseSchema(BaseModel):
+    likes: list[LikeMovieListItemSchema]
+
+    model_config = ConfigDict(from_attributes=True)
