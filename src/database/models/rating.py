977c89453512abd3d171d795dafac79a5eeeb7fe
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-22 12:53:07 +0300

    Add model LikeMovieModel

---
+++ src/database/models/rating.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from src.database.models.base import Base
+
+
+class LikeMovieModel(Base):
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer, ForeignKey("movies.id", ondelete="CASCADE"))
+    profile_id = Column(
+        Integer,
+        ForeignKey("user_profiles.id", ondelete="CASCADE")
+    )
+    movie = relationship("MovieModel", back_populates="likes")
+    profile = relationship("UserProfileModel", back_populates="movie_likes")
