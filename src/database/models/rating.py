6db16edce3aacc263a9fd0b8b89b159d4edb216c
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-22 13:25:37 +0300

    Add relationship movie to RateModel

--- src/database/models/rating.py
+++ src/database/models/rating.py
@@ -61,3 +61,4 @@ class RateModel(Base):
     id = Column(Integer, primary_key=True)
     movie_id = Column(Integer, ForeignKey("movies.id", ondelete="CASCADE"))
     scale = Column(Integer, nullable=False)
+    movie = relationship("MovieModel", back_populates="rates")
