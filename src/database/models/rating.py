9dc2f5bbeb6549cd79ad05c47808b968b2a18dc1
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-09-22 12:55:37 +0300

    Add attribute __tablename__ to LikeMovieModel

--- src/database/models/rating.py
+++ src/database/models/rating.py
@@ -5,6 +5,8 @@ from src.database.models.base import Base
 
 
 class LikeMovieModel(Base):
+    __tablename__ = "movie_likes"
+
     id = Column(Integer, primary_key=True)
     movie_id = Column(Integer, ForeignKey("movies.id", ondelete="CASCADE"))
     profile_id = Column(
